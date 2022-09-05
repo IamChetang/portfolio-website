c5016a1c172c5bdb49fe60b59dae507c6715d9e0
IamChetang <86854700+IamChetang@users.noreply.github.com>
2022-09-05 21:48:17 +0530

    changed navigate

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./Navbar.scss";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-light">
